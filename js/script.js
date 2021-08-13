7a2d065752467cae91e231dfcc62bb6dcc96a217
Ruslan <sidbarret@mail.ru>
2021-08-13 06:57:30 +0300

    Конец урока 14, циклы

--- js/script.js
+++ js/script.js
@@ -1,37 +1,25 @@
 "use strict";
 
-if (-1) {
-    console.log('OK!');
-} else {
-console.log("Ошибка");
-}
 
 
 
-// if (num<49){
-//     console.log("Ошибка");
-
-// } else if (num>100){
-//     console.log("Много");
-// }
-// else {
-//     console.log("Последний вариант!");
+// while(num<=55){
+//     console.log(num);
+//     num++;
 // }
 
-// (num===50) ? console.log('OK!') : console.log("Ошибка");
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while(num<55);
+let num=50;
 
-const num=50;
-switch(num){
-    case 49:
-        console.log("Неверно!");
-        break;
-    case 100:
-        console.log("Неверно!");
-        break;
-    case 51:
-        console.log("В точку!");
-        break;
-    default:
-        console.log("Не в этот раз!");
-        break;
+for(let i =1;i<10;i++){
+
+     if (i===6){
+          //break;
+          continue;
+     }
+     console.log(i);
 }
