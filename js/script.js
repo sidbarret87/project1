63246cdd47559e95da898652f5be02449a20d6a1
Ruslan <sidbarret@mail.ru>
2021-08-15 20:45:55 +0300

    до 10 46 функции 16 урок

--- js/script.js
+++ js/script.js
@@ -1,25 +1,15 @@
 "use strict";
 
+let num=20;
 
+function showFirstMessage(text) {
 
+    console.log(text);
+    console.log(num);
+}
+showFirstMessage("Джеки Чан");
+console.log(num);
 
-// while(num<=55){
-//     console.log(num);
-//     num++;
-// }
+function calc(a,b) {
 
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while(num<55);
-let num=50;
-
-for(let i =1;i<10;i++){
-
-     if (i===6){
-          //break;
-          continue;
-     }
-     console.log(i);
 }
